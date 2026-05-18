21b3c59a1d262dd503a6ed0aff9db6e2ff728ceb
zoeqye123 <zoe.yeqing@gmail.com>
2026-05-18 18:32:02 +0800

    Initial Commit

---
+++ travelclip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  travelclip
+//
+//  Created by moc on 2026/5/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ travelclip/travelclipApp.swift
@@ -0,0 +1,17 @@
+//
+//  travelclipApp.swift
+//  travelclip
+//
+//  Created by moc on 2026/5/18.
+//
+
+import SwiftUI
+
+@main
+struct travelclipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
